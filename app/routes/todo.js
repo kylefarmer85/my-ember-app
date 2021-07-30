b56774d5a7470b698d42d1d15031a3f87082c459
kylefarmer85 <kylefarmer85@gmail.com>
2021-07-29 19:56:31 -0500

    change dummy todos from array of objects to array of strings

--- app/routes/todo.js
+++ app/routes/todo.js
@@ -2,24 +2,7 @@ import Route from '@ember/routing/route';
 
 export default class TodoRoute extends Route {
   model() {
-    return [
-      {
-        id: 1,
-        todo: 'go to the store',
-      },
-      {
-        id: 2,
-        todo: 'walk the dog',
-      },
-      {
-        id: 3,
-        todo: 'call mom',
-      },
-      {
-        id: 4,
-        todo: 'make dinner',
-      },
-    ];
+    return ['go to the store', 'walk the dog', 'call mom', 'make dinner'];
   }
 }
 
